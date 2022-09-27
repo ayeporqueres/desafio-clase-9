b439535d6c17d41c1945464868e7c85b71b4ceb5
JuanIsa <JuanIsa>
2022-09-27 17:55:21 -0300

    1.2.0

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,9 +3,65 @@ import { Link } from 'react-router-dom';
 import { GlobalContext } from './CartContext';
 import './styles/ItemCart.css';
 import imagenes from '../assets/img';
+import { getFirestore } from "../firebase/firebase.config";
+
+import { doc, updateDoc } from "firebase/firestore";
 
 const Cart = () => {
     const { carrito, removeItem, clear, totalCart } = useContext(GlobalContext);
+
+    function terminarCompra() {
+        let usuario = {
+            buyer: {
+                name: 'some user',
+                phone: '32132-133213-2132',
+                email: 'example@email.com'
+            },
+            items: carrito.map(item => {
+                return (
+                    {
+                        id: item.id,
+                        title: item.descripcion,
+                        price: item.precio,
+                        quantity: item.quantity
+                    }
+                );
+            }),
+            date: new Date(),
+            total: totalCart()
+        }
+        generarPedido(usuario);
+        actualizarStock();
+    }
+
+    function generarPedido(datosCompraTerminada) {
+        const db = getFirestore();
+        const itemCollection = db.collection("orders");
+        itemCollection.add(datosCompraTerminada)
+            .then(res => {
+                alert('Se generó un pedido con el id: ' + res.id)
+                clear();
+            })
+            .catch(err => console.log(err))
+    }
+
+    function actualizarStock() {
+        const db = getFirestore();
+        const itemCollection = db.collection("items");
+        itemCollection.get()
+            .then(res => {
+                res.forEach(item => {
+                    carrito.forEach(el => {
+                        if (item.data().id === el.id) {
+                            const consulta = doc(db, "items", item.id);
+                            updateDoc(consulta, { stock: el.stock - el.quantity });
+                        }
+                    });
+                });
+            })
+            .catch(error => console.log(error))
+    }
+
     return (
         <div>
             {carrito.length > 0 ?
@@ -17,10 +73,10 @@ const Cart = () => {
                 </div> :
                 <div className='headerCarrito'>
                     <h2>Tenés el carrito vacio</h2>
-                    <Link  to='/'>Volver a la tienda</Link>
+                    <Link to='/'>Volver a la tienda</Link>
                 </div>
             }
-          
+
             <section>
                 {carrito.map((item) => {
                     return (
@@ -40,7 +96,7 @@ const Cart = () => {
             {carrito.length > 0 &&
                 <div className='itenCardContainer'>
                     El total de tu compra es: U$S {totalCart()}
-                    <button>Terminar la compra</button>
+                    <button onClick={terminarCompra}>Terminar la compra</button>
                 </div>}
         </div>
     );
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDjH52srL4Mzf9jrdAbNvFWBXOqjdSiUEA",
     authDomain: "jts-ecommers.firebaseapp.com",
